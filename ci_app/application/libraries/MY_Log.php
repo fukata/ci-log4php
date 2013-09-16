e615801ff0ceae6541fc78d713838c47529142a9
Ramjee Ganti <ramjee@justeat.in>
2013-09-16 11:29:36 +0530

    Added support for multiple loggers. log4php.xml is the default file for fetching properties as the ini  format does not support all features. 	new file:   ci_app/application/config/log4php.xml 	modified:   ci_app/application/helpers/log4php_helper.php 	modified:   ci_app/application/libraries/MY_Log.php

--- ci_app/application/libraries/MY_Log.php
+++ ci_app/application/libraries/MY_Log.php
@@ -2,50 +2,66 @@
 
 require_once APPPATH . 'third_party/ci_log4php/Logger.php';
 class MY_Log extends CI_Log {
-	
+
 	protected $logger;
+	protected $loggers = array();
 	protected $initialized = false;
-	
+
 	public function __construct() {
 		parent::__construct();
-		
+
+		$log_config_file = 'log4php.xml';
 		if ($this->initialized === false) {
 			$this->initialized = true;
-			$config_file = APPPATH . 'config/log4php.properties';
-			if ( defined('ENVIRONMENT') && file_exists( APPPATH . 'config/' . ENVIRONMENT . '/log4php.properties' ) ) {
-				$config_file = APPPATH . 'config/' . ENVIRONMENT . '/log4php.properties';
+			$config_file = APPPATH . 'config/'.$log_config_file;
+			if ( defined('ENVIRONMENT') && file_exists( APPPATH . 'config/' . ENVIRONMENT . '/'.$log_config_file ) ) {
+				$config_file = APPPATH . 'config/' . ENVIRONMENT . '/'.$log_config_file;
 			}
 			Logger::configure($config_file);
 			$this->logger = Logger::getRootLogger();
+			$this->loggers['root'] = $this->logger;
 		}
 	}
-	
-	public function write_log($level = 'error', $msg, $php_error = FALSE) {
+
+	public function write_log($level = 'error', $msg, $php_error = FALSE, $logger_names='root') {
 		if ($this->_enabled === FALSE) {
 			return FALSE;
 		}
-		
+
 		$level = strtoupper($level);
 
 		if ( ! isset($this->_levels[$level]) OR ($this->_levels[$level] > $this->_threshold) ) {
 			return FALSE;
 		}
-		
-		switch ($level) {
-			case 'ERROR':
-				$this->logger->error($msg);
-				break;
-			case 'INFO':
-				$this->logger->info($msg);
-				break;
-			case 'DEBUG':
-				$this->logger->debug($msg);
-				break;
-			default:
-				$this->logger->debug($msg);
-				break;
+
+		if(!is_array($logger_names)) {
+			$logger_names = array($logger_names);
 		}
-		
+		foreach($logger_names as $logger_name) {
+			if(!isset($this->loggers[$logger_name])) {
+				$this->loggers[$logger_name] = Logger::getLogger($logger_name);
+			}
+
+			$logger = $this->loggers[$logger_name];
+			switch ($level) {
+				case 'ERROR':
+					$logger->error($msg);
+					break;
+				case 'INFO':
+					$logger->info($msg);
+					break;
+				case 'DEBUG':
+					$logger->debug($msg);
+					break;
+				case 'FATAL':
+					$logger->fatal($msg);
+					break;
+				default:
+					$logger->debug($msg);
+					break;
+			}
+
+		}
 		return TRUE;
 	}
 }
